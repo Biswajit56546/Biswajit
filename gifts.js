efc4713d6091555523d1324c7eb2bf958ad90a0a
Biswajit56546 <statuspikachu@gmail.com>
2024-09-12 14:31:32 +0530

    gifts.js

--- gifts.js
+++ gifts.js
@@ -26,7 +26,27 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+function toggleMenu(event) {
+    event.stopPropagation(); 
+    const dropdown = document.querySelector('.content');
+    const menu = document.querySelector('.menu');
+    dropdown.classList.toggle('show');
+    menu.classList.toggle('open');
+}
 
+window.onclick = function(event) {
+    const dropdown = document.querySelector('.content');
+    const menu = document.querySelector('.menu');
+    
+    if (!event.target.matches('.menu') && !dropdown.contains(event.target)) {
+        dropdown.classList.remove('show');
+        menu.classList.remove('open');
+    }
+};
+
+document.querySelector('.content').onclick = function(event) {
+    event.stopPropagation();
+};
 
 function changeTextAndPosition(buttonId, top, left) {
     const div = document.getElementById(buttonId);
